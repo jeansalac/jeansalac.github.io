0f56341a50303d8395e869279d2fb2fefb07f6e8
Jean Salac <jms2wz@virginia.edu>
2025-04-30 15:48:55 -0500

    added recursion analysis slides & starter code

---
+++ classes/cs201_data_structures/starter_code/recursionAnalysis.kt
@@ -0,0 +1,29 @@
+// Iterative Approach to findSum
+// Given a number, sum the the numbers from 0 to n
+fun findSumItr(inputNum: Int): Int{
+    return 0
+
+}
+
+// Recursive Approach to findSum
+// Given a number, sum the the numbers from 0 to n
+fun findSumRecur(inputNum: Int): Int{
+    return 0
+
+}
+
+fun main(){
+
+    // Testing findSumItr()
+    println("Iterative Solution:${findSumItr(20)}")
+    println("Iterative Solution:${findSumItr(14)}")
+    println("Iterative Solution:${findSumItr(8)}")
+    println("Iterative Solution:${findSumItr(0)}")
+
+    // Testing findSumRecur()
+    println("Recursive Solution:${findSumRecur(20)}")
+    println("Recursive Solution:${findSumRecur(14)}")
+    println("Recursive Solution:${findSumRecur(8)}")
+    println("Recursive Solution:${findSumRecur(0)}")
+
+}
