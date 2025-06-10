fd1e64f3023b63b6de16255a652c4097513e9596
Jean Salac <jms2wz@virginia.edu>
2025-06-10 16:32:23 -0500

    added inheritance & interfaces lab to revised data structures schedule

---
+++ classes/cs201_data_structures/starter_code/InheritanceAndInterfacesLab.kt
@@ -0,0 +1,40 @@
+// Test your solutions to Prep Question 1
+open class Person(name: String)
+
+class Student(name:String)/*YOUR CODE HERE*/{
+    /*YOUR CODE HERE */
+}
+
+interface RegistrarRecord{
+    var courses: List<String>
+    fun printCourses()
+}
+
+class StudentRecord/*YOUR CODE HERE*/{
+    /*YOUR CODE HERE*/
+
+}
+
+class ProfessorRecord/*YOUR CODE HERE*/{
+    /*YOUR CODE HERE*/
+
+}
+
+fun main(){
+    // Un-Comment the lines below to test your solution to Prep Question 1
+
+    // var student1 = Student("Alex")
+    // student1.classYear = 2025
+    // student1.declareMajor("Tightrope Walking")
+    // student1.declareMajor("Fire-Eating")
+    // student1.declareMajor("Juggling")
+
+    // Un-Comment the lines below to test your solution to Prep Question 2
+    // var student1 = Student()
+    // student1.courses = mutableListOf("PSYC200", "CS257", "EDUC262")
+    // student1.printCourses()
+
+    // var jean = Professor()
+    // jean.courses = mutableListOf("CS201")
+    // jean.printCourses()
+}
