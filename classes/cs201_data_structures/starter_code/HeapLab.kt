16c57364d4f896c843c09c4e38c24547ccd9c53a
Jean Salac <jms2wz@virginia.edu>
2025-06-11 14:22:25 -0500

    swapped heap lab with percolate up & down version

--- classes/cs201_data_structures/starter_code/HeapLab.kt
+++ classes/cs201_data_structures/starter_code/HeapLab.kt
@@ -1,80 +1,128 @@
-// Kotlin version of Heap code from
-// https://www.programiz.com/dsa/heap-data-structure
-// Ported by Dave Musicant
-// Adapted by Jean for in-class lab
+// Heap Implementation with Percolate Up & Down
 
 interface Heap{
-    fun heapify(hT: MutableList<Int>, i: Int)
-    fun insert(hT: MutableList<Int>, newNum: Int)
-    fun deleteNode(hT: MutableList<Int>, num: Int)
+    fun heapify(list: MutableList<Int>)
+    fun insert(list: MutableList<Int>, newNum: Int)
+    fun delete(list: MutableList<Int>, num: Int)
 }
 
-// MaxHeap Implementation
+// Max-Heap Implementation
 class MaxHeap: Heap {
-    override fun heapify(hT: MutableList<Int>, i: Int) {
-        var largest = i
-        var l = 2 * i + 1
-        var r = 2 * i + 2
-        if (l < hT.count() && hT.get(l) > hT.get(largest))
-            largest = l
-        if (r < hT.count() && hT.get(r) > hT.get(largest))
-            largest = r
+    fun percolate(list: MutableList<Int>, curNode: Int, swapNode: Int){
+        var tmp = list[curNode]
+        list[curNode] = list[swapNode]
+        list[swapNode] = tmp
+    }
+    
+    override fun heapify(list: MutableList<Int>) {
+        // Start with parent of rightmost leaf node
+        for(curNode in (list.size/2) - 1 downTo 0){
+            // Compare current node with its parent
+            
+            // Check if current node is a left or a right child
+            var parent = if (/*YOUR CODE HERE */) /*YOUR CODE HERE */ else /*YOUR CODE HERE */
 
-        if (largest != i) {
-            val temp = hT.get(largest)
-            hT.set(largest, hT.get(i))
-            hT.set(i, temp)
+            // Check if parent is in bounds
+            if(/*YOUR CODE HERE */){
+                // If current node is larger than its parent, percolate up
+                if(/*YOUR CODE HERE */){
+                    percolate(/*YOUR CODE HERE */)
+                }
+            }
 
-            heapify(hT, largest)
+            // Compare current node with its children
+            var leftChild = /*YOUR CODE HERE */
+            var rightChild = /*YOUR CODE HERE */
+
+            // First, check if children are in bounds
+            // If current node is smaller than its children, percolate down
+            if(/*YOUR CODE HERE */){
+                if(/*YOUR CODE HERE */){
+                    percolate(/*YOUR CODE HERE */)
+                }
+            }
+
+            if(/*YOUR CODE HERE */){
+                if(/*YOUR CODE HERE */){
+                    percolate(/*YOUR CODE HERE */)
+                }
+            }
+            
         }
     }
 
-    override fun insert(hT: MutableList<Int>, newNum: Int) {
-        val size = hT.count()
-        if (size == 0) {
-            hT.add(newNum)
-        } else {
-            hT.add(newNum)
-            for (i in (size/2-1) downTo 0) {
-                heapify(hT, i)
+    override fun insert(list: MutableList<Int>, newNum: Int){
+        if(list.size == 0 ){
+            list.add(newNum)
+        }
+
+        else if(list.size == 1){
+            list.add(newNum)
+            if (/*YOUR CODE HERE */){
+                percolate(/*YOUR CODE HERE */)
             }
         }
+        
+        else{
+            list.add(newNum)
+            var newNumIndex = list.size - 1
+            var parent = if (newNumIndex%2 == 1) (newNumIndex-1) / 2 else (newNumIndex-2) / 2
+
+            // While new node is larger than its parent, percolate up
+            while(/*YOUR CODE HERE */){
+                percolate(/*YOUR CODE HERE */)
+                newNumIndex = /*YOUR CODE HERE */
+
+                //If you're at the root, break
+                if (newNumIndex == 0){
+                    break
+                } else{
+                    parent = if (/*YOUR CODE HERE */) /*YOUR CODE HERE */ else /*YOUR CODE HERE */
+                }
+            }
+        }   
     }
 
-    override fun deleteNode(hT: MutableList<Int>, num: Int) {
-        val size = hT.count()
+    override fun delete(list: MutableList<Int>, num: Int){
         var deleteIndex: Int = -1
-        for (i in 0..<size) {
-            if (num == hT.get(i)) {
+
+        // Find the item to be deleted
+        for (i in 0..<list.size) {
+            if (num == list.get(i)) {
                 deleteIndex = i
                 break
             }
         }
 
-        var temp = hT.get(deleteIndex)
-        hT.set(deleteIndex, hT.get(size-1))
-        hT.set(size-1, temp)
+        // Move the last item in the heap into the index of the deleted node
+        list[deleteIndex] = /*YOUR CODE HERE */
+        list.removeLast()
 
-        hT.remove(size-1)
-        for (j in size/2-1 downTo 0)
-            heapify(hT, j)
+        // Re-heapify
+        heapify(list)
+        
     }
+
 }
 
-// Implement Min-Heap based on Max-Heap code above
+fun main(){
+    val listToHeap = mutableListOf<Int>()
 
-fun main() {
-    // List representation of Min-Heap from Prep
-    val list = listOf(4, 23, 12, 35, 36, 35, 14, 39, 42)
+    val maxHeap = MaxHeap()
 
-    // Initalize a Min-Heap object
+    maxHeap.insert(listToHeap, 3)
 
-    // Insert 11 into the Min-Heap and print out the list
+    maxHeap.insert(listToHeap, 4)
 
-    // Insert 2 into the Min-Heap and print out the list
+    maxHeap.insert(listToHeap, 9)
 
-    // Delete 14 from the Min-Heap and print out the list
+    maxHeap.insert(listToHeap, 5)
 
-    // Delete 4 from the Min-Heap and print out the list
+    maxHeap.insert(listToHeap, 2)
+    println(listToHeap)
+
+    maxHeap.delete(listToHeap, 9)
+    println(listToHeap)
+    
 
 }
