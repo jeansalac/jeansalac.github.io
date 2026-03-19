31cbb46aa583ee958aa473356777ca4b0efeeb4a
Jean Salac <jms2wz@virginia.edu>
2026-03-19 18:25:28 -0500

    added starter code for debugging lab

---
+++ classes/cs111_intro_cs/starter_code/lms.py
@@ -0,0 +1,6 @@
+username = User 
+print("Hello" username)
+class = "Intro to CS"
+print("Welcome to ", class)
+numHW = 2
+print("Number of homework assignments due today:" + numHW)
---
+++ classes/cs111_intro_cs/starter_code/lyrics.py
@@ -0,0 +1,13 @@
+print("We're going")
+
+for i in range(0,3):
+    print("up")
+
+print("It's our moment")
+print("You know together we're glowing, ")
+
+gonnaBe = "gonna be"
+for i in range(0,2):
+    print(gonnaBe)
+
+print("golden!")
