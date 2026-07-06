a25d6478323b20d52812fb3dfe635866a7d9c5d9
Jean Salac <jms2wz@virginia.edu>
2026-07-06 15:33:09 -0500

    Updated basic_cl.py

--- classes/cs257_software_design/basic_cl.py
+++ classes/cs257_software_design/basic_cl.py
@@ -4,7 +4,9 @@ import sys
 data = []
 
 def load_data():
-
+    '''Arguments: None
+    Return value: None
+    Purpose: Loads the data from a file'''
     data.clear()
     with open('Data/dataset.csv', newline='') as f:
         reader = csv.reader(f)
@@ -12,17 +14,43 @@ def load_data():
             data.append(row)
 
 def get_cell(row, column):
+    '''Arguments: row (int), column (int)
+    Return value: the value at that cell
+    Purpose: get the value at a specified cell
+    Raises: IndexError if row or column outside of bounds'''
+    
     if row >= len(data):
         raise IndexError("Row argument invalid")
     if column >= len(data[0]):
         raise IndexError("Column argument invalid")
     return data[int(row)][int(column)]
 
+def get_row_titles():
+    '''Arguments: None
+    Return: List of row titles
+    Purpose: Get a list of the row titles
+    '''
+    row_titles = []
+    for row in data:
+        row_titles.append(row[0])
+    return row_titles
+
 def get_row_by_title(title):
+    '''Arguments: title (string)
+    Return value: list of values of a row based on the title (yes, a row, not a column as you would expect :) ); if row title isn't in table, returns empty list
+    Purpose: to get a row based on the first value in that row
+    '''
     for row in data:
         if row[0] == title:
             return row
     return []
+
+def get_silly():
+    '''Arguments: None
+    Return value: 2
+    Purpose: just a simple silly method
+    '''
+    return 2
 
 def main():
     load_data()
